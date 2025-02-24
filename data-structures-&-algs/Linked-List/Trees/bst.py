d35193a403e6c76da0470e669b7cf99ab5172e24
Lemar Tokham <lemartokham@Lemars-MacBook.local>
2025-02-24 13:38:58 +0000

    Add Search functionality

--- data-structures-&-algs/Linked-List/Trees/bst.py
+++ data-structures-&-algs/Linked-List/Trees/bst.py
@@ -5,6 +5,24 @@ class TreeNode:
         self.right = None
         self.left = None
 
+def search(root, target):
+    if not root:
+        return False # No rootnode, tree is empty
+    
+    if target > root.val:
+        return search(root.right, target)
+    elif target < root.val:
+        return search(root.left, target)
+    else:
+        return True
+    
+
+
 # Create tree node object
 root = TreeNode(2)
-print(root.val)
+leaf1 = TreeNode(1)
+leaf2 = TreeNode(3)
+root.left = leaf1
+root.right = leaf2
+print(root.val, root.left.val, root.right.val)
+print(search(root, 4))
