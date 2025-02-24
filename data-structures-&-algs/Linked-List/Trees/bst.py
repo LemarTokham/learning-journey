7ed70f0a3d2c3251d5ef5c51a138119216bcb4fa
Lemar Tokham <lemartokham@Lemars-MacBook.local>
2025-02-24 13:29:55 +0000

    Add binary search tree implentation

---
+++ data-structures-&-algs/Linked-List/Trees/bst.py
@@ -0,0 +1,10 @@
+# Binary Search Tree
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.right = None
+        self.left = None
+
+# Create tree node object
+root = TreeNode(2)
+print(root.val)
